42bf5222110a61241384e33ace355aefce8d6fad
Łukasz Kwasek <pamput@gmail.com>
2016-02-20 13:25:01 +0000

    add simple jump\nmodify hold jump field labels

---
+++ Assets/Scripts/CatGirl/CatGirlHoldJumpMovementScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CatGirl {
+
+    [RequireComponent(typeof(CatGirlControllerScript))]
+    public class CatGirlHoldJumpMovementScript : MonoBehaviour {
+
+        public float speed = 15f;
+        public float startJumpVelocity = 15f;
+        public float jumpVelocity = 30f;
+        public float jumpMaxHoldTime = 0.08f;
+
+        bool grounded;
+        bool notInMidJump = true;
+
+        float jumpHoldTime = 0;
+        bool jumpAllow = true;
+         
+        CatGirlControllerScript input;
+        new Rigidbody2D rigidbody2D;
+
+        // Use this for initialization
+        void Start() {
+            input = GetComponent<CatGirlControllerScript>();
+            rigidbody2D = GetComponent<Rigidbody2D>();
+
+            input.OnMove += OnMove;
+            input.OnJump += OnJump;
+            input.OnGrounded += OnGrounded;
+        }
+
+        void OnMove(float rawDirection, float direction) {
+            float distance = rawDirection * speed;
+            rigidbody2D.velocity = new Vector2(distance, rigidbody2D.velocity.y);
+
+            if (distance > 0) {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            } else if (distance < 0) {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
+            }
+        }
+
+        void OnJump(bool buttonJump, float buttonJumpTime) {
+            
+            if (buttonJump && grounded) {
+                rigidbody2D.velocity = Vector2.up * startJumpVelocity;
+            }
+
+            bool fastJump = !grounded && !buttonJump && buttonJumpTime > 0;
+            bool longJump = !grounded && buttonJump && buttonJumpTime >= jumpMaxHoldTime;
+            bool isJump = fastJump || longJump;
+
+            if (isJump && jumpAllow && rigidbody2D.velocity.y > 0) {
+                float fraction = jumpVelocity / jumpMaxHoldTime;
+                float force = fraction * Mathf.Min(jumpMaxHoldTime, buttonJumpTime);
+                force = Mathf.Max(force, rigidbody2D.velocity.y);
+                rigidbody2D.velocity = Vector2.up * force;
+                jumpAllow = false;
+            }
+        }
+
+        void OnGrounded(bool grounded) {
+            this.grounded = grounded;
+
+            if (grounded) {
+                jumpAllow = true;
+            }
+
+            if (grounded && rigidbody2D.velocity.y < 0) {
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0);
+            }
+        }
+    }
+}
--- Assets/Scripts/CatGirl/CatGirlSimpleMovementScript.cs
+++ Assets/Scripts/CatGirl/CatGirlSimpleMovementScript.cs
@@ -4,19 +4,14 @@ using System.Collections;
 namespace CatGirl {
 
     [RequireComponent(typeof(CatGirlControllerScript))]
-    public class CatGirlMovementScript : MonoBehaviour {
+    public class CatGirlSimpleMovementScript : MonoBehaviour {
 
         public float speed = 15f;
-        public float startJumpForce = 15f;
-        public float jumpForce = 30f;
-        public float jumpMaxHoldTime = 0.08f;
+        public float jumpForce = 1500f;
 
         bool grounded;
-        bool notInMidJump = true;
+        bool canJump = true;
 
-        float jumpHoldTime = 0;
-        bool jumpAllow = true;
-         
         CatGirlControllerScript input;
         new Rigidbody2D rigidbody2D;
 
@@ -42,30 +37,20 @@ namespace CatGirl {
         }
 
         void OnJump(bool buttonJump, float buttonJumpTime) {
-            
-            if (buttonJump && grounded) {
-                rigidbody2D.velocity = Vector2.up * startJumpForce;
-            }
+            if (grounded) {
+                if (!buttonJump) {
+                    canJump = true;
+                }
 
-            bool fastJump = !grounded && !buttonJump && buttonJumpTime > 0;
-            bool longJump = !grounded && buttonJump && buttonJumpTime >= jumpMaxHoldTime;
-            bool isJump = fastJump || longJump;
-
-            if (isJump && jumpAllow && rigidbody2D.velocity.y > 0) {
-                float fraction = jumpForce / jumpMaxHoldTime;
-                float force = fraction * Mathf.Min(jumpMaxHoldTime, buttonJumpTime);
-                force = Mathf.Max(force, rigidbody2D.velocity.y);
-                rigidbody2D.velocity = Vector2.up * force;
-                jumpAllow = false;
+                if (buttonJump && canJump) {
+                    rigidbody2D.AddForce(Vector2.up * jumpForce);
+                    canJump = false;
+                }
             }
         }
 
         void OnGrounded(bool grounded) {
             this.grounded = grounded;
-
-            if (grounded) {
-                jumpAllow = true;
-            }
 
             if (grounded && rigidbody2D.velocity.y < 0) {
                 rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0);
